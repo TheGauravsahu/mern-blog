e99b48a11bc8ebc392912b34d342e11d5e5ca8ba
Gaurav Sahu <itsgauravsahu7@gmail.com>
2025-01-09 22:47:34 +0530

    completed user signin

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,75 +1,120 @@
 import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Card } from "@/components/ui/card";
+import axios from "@/config/axios";
+import { showToast } from "@/config/toastify";
 
 const SignIn = () => {
-  const [formData, setFormData] = useState({
-    email: "",
-    password: "",
+  const navigate = useNavigate();
+
+  const formSchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(6, "Password must be 6 characters long."),
   });
 
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.id]: e.target.value });
-  };
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    // Add your sign-in logic here
+  const handleSubmit = async (values) => {
+    try {
+      const response = await axios.post("/users/login", values);
+      const data = response.data;
+      showToast("success", data.message);
+      navigate("/");
+    } catch (error) {
+      const errorMessage = error.response?.data?.message || "An error occurred";
+      showToast("error", errorMessage);
+    }
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-50">
-      <div className="max-w-md w-full space-y-8 p-8 bg-white rounded-lg shadow-md">
-        <div>
-          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+    <div className="h-screen w-screen flex items-center justify-center bg-gray-50">
+      <Card className="w-[400px] p-5 h-">
+        <div className="my-8">
+          <div className="flex items-center justify-center mb-1">
+            <Link to="/" className="text-center text-xl">
+              MERN blogs
+            </Link>
+          </div>
+          <h2 className="text-center text-3xl font-semibold text-gray-900">
             Sign in to your account
           </h2>
         </div>
-        <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
-          <div className="rounded-md shadow-sm space-y-4">
-            <div>
-              <label htmlFor="email" className="sr-only">
-                Email address
-              </label>
-              <Input
-                id="email"
+        <Form {...form}>
+          <form
+            onSubmitclassName="mt-8 space-y-6"
+            onSubmit={form.handleSubmit(handleSubmit)}
+          >
+            <div className="mb-3">
+              <FormField
+                control={form.control}
                 name="email"
-                type="email"
-                required
-                placeholder="Enter you email address"
-                onChange={handleChange}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input {...field} placeholder="Enter you email address" />
+                    </FormControl>
+                    <FormMessage className="font-medium" />
+                  </FormItem>
+                )}
               />
             </div>
-            <div>
-              <label htmlFor="password" className="sr-only">
-                Password
-              </label>
-              <Input
-                id="password"
+
+            <div className="mb-3">
+              <FormField
+                control={form.control}
                 name="password"
-                type="password"
-                required
-                placeholder="Enter your password"
-                onChange={handleChange}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="password"
+                        placeholder="Enter your password"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage className="font-medium" />
+                  </FormItem>
+                )}
               />
             </div>
-          </div>
 
-          <div>
-            <Button type="submit" className="w-full">
-              Sign in
-            </Button>
-          </div>
-        </form>
+            <div className="my-8">
+              <Button type="submit" className="w-full">
+                Sign in
+              </Button>
+            </div>
 
-        <p className="text-sm text-center">
-          Don't have an account?{" "}
-          <Link to="/signup" className="text-blue-500">
-            Sign up
-          </Link>
-        </p>
-      </div>
+            <div>
+              <p className="text-sm text-center">
+                Don't have an account?{" "}
+                <Link to="/signup" className="text-blue-500">
+                  Sign up
+                </Link>
+              </p>
+            </div>
+          </form>
+        </Form>
+      </Card>
     </div>
   );
 };
