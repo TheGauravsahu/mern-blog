0074526e74368700ef54e7ccffd249a27d5dfd20
Gaurav Sahu <itsgauravsahu7@gmail.com>
2025-01-09 18:56:28 +0530

    add login

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,9 +1,15 @@
+import { validationResult } from "express-validator";
 import { errorHandler } from "../helpers/ErrorHandler.js";
 import userModel from "../models/user.model.js";
 import * as userService from "../services/user.service.js";
 
 export const registerUser = async (req, res, next) => {
   try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    
     const { name, email, password } = req.body;
 
     const userExists = await userModel.findOne({ email });
@@ -28,6 +34,41 @@ export const registerUser = async (req, res, next) => {
       token,
       user: userResponse,
       message: "Registration successful.",
+    });
+  } catch (error) {
+    next(errorHandler(400, error.message));
+  }
+};
+
+export const loginUser = async (req, res, next) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    const user = await userModel.findOne({ email }).select("+password");
+
+    if (!user) {
+      next(errorHandler(404, "Invalid email or password."));
+    }
+
+    const isMatch = await user.comparePassword(password);
+    if (!isMatch) {
+      next(errorHandler(400, "Invalid email or password."));
+    }
+
+    const token = user.generateJWT();
+
+    const userResponse = user.toObject();
+    delete userResponse.password;
+
+    return res.status(200).json({
+      token,
+      user: userResponse,
+      message: "Login successful.",
     });
   } catch (error) {
     next(errorHandler(400, error.message));
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,8 +1,29 @@
 import { Router } from "express";
 import * as userController from "../controllers/user.controller.js";
+import { body } from "express-validator";
 
 const router = Router();
 
-router.post("/signup", userController.registerUser);
+router.post(
+  "/signup",
+  [
+    body("name").notEmpty().withMessage("Name is required."),
+    body("email").isEmail().withMessage("Valid email is required."),
+    body("password")
+      .isLength({ min: 6 })
+      .withMessage("Password must be at least 6 characters long."),
+  ],
+  userController.registerUser
+);
+router.post(
+  "/login",
+  [
+    body("email").isEmail().withMessage("Valid email is required."),
+    body("password")
+      .isLength({ min: 6 })
+      .withMessage("Password must be at least 6 characters long."),
+  ],
+  userController.loginUser
+);
 
-export default router
+export default router;
