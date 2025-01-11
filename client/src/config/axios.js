d1cf53b5887537caad0a1c68f82d13cf2bf3d886
Gaurav Sahu <itsgauravsahu7@gmail.com>
2025-01-11 11:48:07 +0530

    Fix: Use Axios Interceptors for Dynamic Authorization Header

--- client/src/config/axios.js
+++ client/src/config/axios.js
@@ -2,9 +2,21 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  headers: {
-    Authorization: `Bearer ${localStorage.getItem("token")}`,
-  },
 });
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    } else {
+      delete config.headers.Authorization; // Remove Authorization header if no token
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default axiosInstance;
