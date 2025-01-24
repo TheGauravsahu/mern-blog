43f4ef359fd179dc4a4c0ccaf0fb25fa990f5a87
Gaurav Sahu <itsgauravsahu7@gmail.com>
2025-01-24 09:05:38 +0530

    feat : add admin protected 👮

---
+++ client/src/components/admin-protected.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { Navigate, Outlet } from "react-router-dom";
+
+const AdminProtected = () => {
+  const user = useSelector((state) => state.user);
+  if (user && user.isLoggedIn && user.user.role === "admin") {
+    return <Outlet />;
+  } else {
+    return <Navigate to="/signin" />;
+  }
+};
+
+export default AdminProtected;
